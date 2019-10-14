3659bad6e07a128d120cc9833ea7801aff027aca
ZhuravlevaTatiana <guravleva@gro-rm.ru>
2019-10-14 15:28:30 +0300

    проба

--- js/main.js
+++ js/main.js
@@ -1,52 +1,66 @@
 'use strict';
-var activeMap = document.querySelector('.map');
-activeMap.classList.remove('.map--faded');
-var DATES_LENGTH = 8;
-var random = function (min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
-var getRandomItemFromArray = function (arr) {
-  return arr[random(0, arr.length - 1)];
-};
-var createRandomDate = function () {
-  var dateArray = [];
-  for (var i = 0; i <= DATES_LENGTH; i++) {
-    dateArray.push({
-      author: {
-        avatar: getRandomItemFromArray(),
-      },
-      offer: {
-        title: getRandomItemFromArray(),
-        address: getRandomItemFromArray(),
-        price: getRandomItemFromArray(),
-        type: getRandomItemFromArray(),
-        rooms: getRandomItemFromArray(),
-        guests: getRandomItemFromArray(),
-        checkin: getRandomItemFromArray(),
-        checkout: getRandomItemFromArray(),
-        features: getRandomItemFromArray(),
-        description: getRandomItemFromArray(),
-        photos: getRandomItemFromArray(),
-      },
-      location: {
-        x: getRandomItemFromArray(),
-        y: getRandomItemFromArray(),
-      }
-    });
+var itemUnique = function (array, item) {
+  for (var i = 0; i < array.length; i++) {
+    if (array(i) === item) {
+      return false;
+    }
   }
-  return dateArray;
+  return true;
 };
-var dates = createRandomDate();
-var similarListElement = document.querySelector('.map__pin');
-var similarDateTemplate = document.querySelector('#pin');
-var render = function (date) {
-  var dateElement = similarDateTemplate.cloneNode(true);
-  dateElement.querySelector('.').textContent = date.location;
-  return dateElement;
+var getRandomFromArr = function (array, count) {
+  var result = [];
+  for (var i = 0; i < count || i < array.length; i++) {
+    var randomItem = array[getRandomNumber(0, array.length - 1)];
+    if (itemUnique(result, randomItem)) {
+      result.push(randomItem);
+    }
+  }
+  return result;
 };
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < dates.length; i++) {
-  fragment.appendChild(render(dates[i]));
-}
-similarListElement.appendChild(fragment);
+var generateAd = function () {
+  var ads = [];
+  var ADS_NUMBER = 8;
+  var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+  var CHECK_TIMES = ['12:00', '13:00', '14:00'];
+  var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var OFFER_PHOTOS = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ];
+  for (var i = 0; i < ADS_NUMBER; i++) {
+    var locationX = Math.floor(Math.random() * (mapWidth - PIN_WIDTH));
+    var locationY = 130 + Math.floor(Math.random() * 500);
+    var featuresNumber = 1 + Math.floor(Math.random() * (OFFER_FEATURES.length - 1));
+    var photosNumber = 1 + Math.floor(Math.random() * (OFFER_PHOTOS.length - 1));
+    var ad = {
+      'author': {
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      'offer': {
+        'title': 'title-' + i,
+        'address': '{{location.x-' + i + '}}, {{location.y-' + i + '}}',
+        'price': '{{price1-' + i + '}}, {{price2-' + i + '}}',
+        'type': getRandomFromArr(OFFER_TYPES, -1),
+        'rooms': '{{rooms-' + i + '}}',
+        'guests': '{{guests-' + i + '}}',
+        'checkin': getRandomFromArr(CHECK_TIMES, -1),
+        'checkout': getRandomFromArr(CHECK_TIMES, -1),
+        'features': getRandomFromArr(OFFER_FEATURES, featuresNumber),
+        'description': '{{descr-' + i + '}}',
+        'photos': getRandomFromArr(OFFER_PHOTOS, photosNumber)
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
+      }
+    };
+    ads.push(ad);
+  }
+  return ads;
+};
+var ads = generateAd();
 
